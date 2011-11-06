03e0e442a31ce020e746f7aa3f031d9fd53a31ef
slodge <lodge.stuart@gmail.com>
2011-11-06 08:31:39 +0000

    restructure

--- Example/RunSpec.js
+++ Example/RunSpec.js
@@ -9,188 +9,215 @@
 // Author - Stuart Lodge, Cirrious. http://www.cirrious.com
 // ------------------------------------------------------------------------
 
+
+function executeTests(config)
+{
+	WScript.echo("Loading helpers...");
+
+	/* helper methods */
+
+	function twoDigits(input) {
+		input = '' + input;
+		while (input.length < 2)
+			input = '0' + input;
+		return input;
+	}
+
+	String.prototype.startsWith = function(str) {
+		return (this.match("^"+str)==str);
+	}
+
+	String.prototype.endsWith = function(str) {
+		return (this.match(str+"$")==str);
+	}
+
+	Date.prototype.prettyPrint = function () {
+		return this.getFullYear() + '-' +
+			twoDigits(this.getMonth()+1) + '-' +
+			twoDigits(this.getDate())  + '-' +
+			twoDigits(this.getHours())  + '-' +
+			twoDigits(this.getMinutes())  + '-' +
+			twoDigits(this.getSeconds());
+	}
+
+	function safeCreateFolder(fso, folderPath) {
+		if (!fso.FolderExists(folderPath))
+			fso.CreateFolder(folderPath);
+	}
+
+	function patchUpReport(reportText) {
+		reportText = reportText.replace( /_startEmuShot_/g, '\n          <img src="');
+		reportText = reportText.replace( /_endEmuShot_/g, '" alt="" height="200"/>');
+		reportText = reportText.replace( / \tTrace:/g, '<span class="emuTraceMessage">');
+		reportText = reportText.replace( / \tWarning:/g, '<span class="emuWarningMessage">');
+		reportText = reportText.replace( / \tError/g, '<span class="emuErrorMessage">');
+		reportText = reportText.replace( / \t\t \t/g, '</span>');
+		reportText = reportText.replace( /span.traceMessage/g, 'span.emuTraceMessage\n{ font-style:italic; margin-left: 4em; color: #888888; }\nspan.emuWarningMessage\n{ font-style:italic; margin-left: 4em; color: #FFB00F; }\nspan.emuErrorMessage\n{ font-style:italic; margin-left: 4em; color: #FF3030; }\nspan.traceMessage' );
+		return reportText;
+	}
+
+	// define file constants
+	// Note: if a file exists, using forWriting will set
+	// the contents of the file to zero before writing to
+	// it. 
+	var forReading = 1, forWriting = 2, forAppending = 8;
+
+	function writeStringIntoFile(f, text) {
+		// Open the file 
+		os = f.OpenAsTextStream( forWriting, 0 );
+		
+		// write the text
+		os.Write(text);
+		
+		//close the file
+		os.Close();
+	}
+
+	function readFileIntoString(f) {
+		// define array to store lines. 
+		rline = new Array();
+
+		// Open the file 
+		is = f.OpenAsTextStream( forReading, 0 );
+		
+		// start and continue to read until we hit
+		// the end of the file. 
+		var count = 0;
+		while( !is.AtEndOfStream ){
+		   rline[count] = is.ReadLine();
+		   count++;
+		}
+		// Close the stream 
+		is.Close();
+		// Place the contents of the array into 
+		// a variable. 
+		var msg = "";
+		for(i = 0; i < rline.length; i++){
+		   msg += rline[i] + "\n";
+		}
+		return msg;
+	}
+
+	function runWithEcho(toRun, title, detailedEcho) {
+		WScript.echo("======================");
+		WScript.echo("Starting: " + title + "...");
+		WScript.echo('');
+		try
+		{
+			WScript.echo(detailedEcho);
+			toRun();
+		}
+		catch (error)
+		{
+			WScript.echo('Error seen: ' + error.description);
+		}
+		WScript.echo("Completed: " + title);
+		WScript.echo('');
+	}
+
+	/* end helpers */
+
+	WScript.echo("");
+	WScript.echo("======================");
+	WScript.echo("ExampleApp Test Runner");
+	WScript.echo("======================");
+	WScript.echo("");
+
+	WScript.echo("Setting up test results folder");
+
+	var fso = WScript.CreateObject("Scripting.FileSystemObject");
+	safeCreateFolder(fso, config.testDirectory);
+
+	var shell = WScript.CreateObject("WScript.Shell");
+	shell.CurrentDirectory = config.testDirectory;
+
+	var date =  new Date();
+	var datedTestDirectory = config.testDirectory + '/' + date.prettyPrint();
+	safeCreateFolder(fso, datedTestDirectory);					
+	shell.CurrentDirectory = datedTestDirectory;
+
+	WScript.echo("Test results folder is " + datedTestDirectory);
+
+	WScript.echo('');
+
+	var batchCommand = '"' + config.pathToNUnitConsole + '"'
+						+ ' "' + config.pathToTargetFolder + config.targetFileName  + '"'
+						+ ' /labels /out=TestResult.txt /xml=TestResult.xml';
+	runWithEcho(function() {
+						WScript.echo(batchCommand);
+						shell.run(batchCommand, 1 /* show normal */, true /* wait for this to finish*/);					
+					},
+					"Running NUnit",
+					"shell>" + batchCommand);				
+
+	var reportCommand = '"' + config.pathToSpecFlow + '"'
+						+ ' nunitexecutionreport' 
+						+ ' "' + config.pathToClientCsProj + '"';
+						
+	runWithEcho(function() {
+						WScript.echo(reportCommand);
+						shell.run(reportCommand, 1 /* show normal */, true /* wait for this to finish*/);
+					},
+					"Generating report",
+					"shell>" + reportCommand);
+					
+	var imageSpec = config.pathToTargetFolder + "_EmuShot_*.png";
+	runWithEcho(function() {
+						fso.MoveFile(imageSpec, datedTestDirectory);
+					},
+				'Moving images',
+				'File source spec is: ' + imageSpec);
+				
+	var reportPath = datedTestDirectory + '/TestResult.html';
+	runWithEcho(function() {
+						var reportFile = fso.GetFile(reportPath);
+						var reportText = readFileIntoString(reportFile);
+						reportText = patchUpReport(reportText);
+						writeStringIntoFile(reportFile, reportText);
+					},
+				'Patching report syntax');
+
+	WScript.echo("======================");
+	WScript.echo('Complete!');
+	WScript.echo('');
+
+
+	if (config.openHtmlInBrowser) {
+		WScript.echo('Opening test result file...');
+		shell.run(reportPath);
+		WScript.echo('');
+		WScript.echo('Report open!');
+		WScript.echo('');
+	}
+}
+
 WScript.echo("Warning - if you are running this using WScript.exe instead of CScript.exe then expect a lot of message boxes!");
 
-WScript.echo("Loading helpers...");
+// script parameters - exe paths
+var relativePathToSpecFlow = '/../packages/SpecFlow.1.8.1/tools/specflow.exe';
+var relativePathToNUnitConsole = '/../packages/NUnit.2.5.10.11092/tools/nunit-console-x86.exe'
 
-/* helper methods */
+// script parameters - target
+var relativePathToTargetFolder = "/../Bin/debug/";
+var targetFileName = 'ExampleApp.Spec.dll';
+var relativePathToClientCsProj = "/ExampleApp/ExampleApp.csproj";
 
-function twoDigits(input) {
-	input = '' + input;
-	while (input.length < 2)
-		input = '0' + input;
-	return input;
-}
-
-String.prototype.startsWith = function(str) {
-	return (this.match("^"+str)==str);
-}
-
-String.prototype.endsWith = function(str) {
-	return (this.match(str+"$")==str);
-}
-
-Date.prototype.prettyPrint = function () {
-	return this.getFullYear() + '-' +
-		twoDigits(this.getMonth()+1) + '-' +
-		twoDigits(this.getDate())  + '-' +
-		twoDigits(this.getHours())  + '-' +
-		twoDigits(this.getMinutes())  + '-' +
-		twoDigits(this.getSeconds());
-}
-
-function safeCreateFolder(folderPath) {
-	if (!fso.FolderExists(folderPath))
-		fso.CreateFolder(folderPath);
-}
-
-function patchUpReport(reportText) {
-	reportText = reportText.replace( /_startEmuShot_/g, '\n          <img src="');
-	reportText = reportText.replace( /_endEmuShot_/g, '" height="200">');
-	reportText = reportText.replace( /^Trace:/g, '<span class="emuTraceMessage">');
-	reportText = reportText.replace( /^Warning:/g, '<span class="emuWarningMessage">');
-	reportText = reportText.replace( /^Error/g, '<span class="emuErrorMessage">');
-	reportText = reportText.replace( / \t\t \t$/g, '</span>');
-	reportText = reportText.replace( /span.traceMessage/g, 'span.emuTraceMessage\n{ font-style:italic; margin-left: 4em; color: #888888; }\nspan.emuWarningMessage\n{ font-style:italic; margin-left: 4em; color: #FFB00F; }\nspan.emuErrorMessage\n{ font-style:italic; margin-left: 4em; color: #FF3030; }\nspan.traceMessage' );
-	return reportText;
-}
-
-// define file constants
-// Note: if a file exists, using forWriting will set
-// the contents of the file to zero before writing to
-// it. 
-var forReading = 1, forWriting = 2, forAppending = 8;
-
-function writeStringIntoFile(f, text) {
-	// Open the file 
-	os = f.OpenAsTextStream( forWriting, 0 );
-	
-	// write the text
-	os.Write(text);
-	
-	//close the file
-	os.Close();
-}
-
-function readFileIntoString(f) {
-	// define array to store lines. 
-	rline = new Array();
-
-	// Open the file 
-	is = f.OpenAsTextStream( forReading, 0 );
-	
-	// start and continue to read until we hit
-	// the end of the file. 
-	var count = 0;
-	while( !is.AtEndOfStream ){
-	   rline[count] = is.ReadLine();
-	   count++;
-	}
-	// Close the stream 
-	is.Close();
-	// Place the contents of the array into 
-	// a variable. 
-	var msg = "";
-	for(i = 0; i < rline.length; i++){
-	   msg += rline[i] + "\n";
-	}
-	return msg;
-}
-
-/* end helpers */
-
-WScript.echo("");
-WScript.echo("======================");
-WScript.echo("ExampleApp Test Runner");
-WScript.echo("======================");
-WScript.echo("");
+// operational parameters
+var openHtmlInBrowser = WScript.Arguments.Named.Exists("openHtml") ? Number(WScript.Arguments.Named.Item("openHtml")) : 1;
 
 var shell = WScript.CreateObject("WScript.Shell");
 var baseDirectory = shell.CurrentDirectory;
 
-WScript.echo("Setting up test results folder");
+// put everything in one config/settings object
+var settings = {
+	testDirectory : baseDirectory + '/Test',
+	pathToNUnitConsole : baseDirectory + relativePathToNUnitConsole,
+	pathToTargetFolder : baseDirectory + relativePathToTargetFolder,
+	pathToSpecFlow : baseDirectory + relativePathToSpecFlow,
+	pathToClientCsProj : baseDirectory + relativePathToClientCsProj,
+	targetFileName : targetFileName,
+	openHtmlInBrowser : openHtmlInBrowser
+};
 
-var fso = WScript.CreateObject("Scripting.FileSystemObject");
-var testDirectory = baseDirectory + '/Test';
-safeCreateFolder(testDirectory);
-
-shell.CurrentDirectory = testDirectory;
-
-var date =  new Date();
-var datedTestDirectory = testDirectory + '/' + date.prettyPrint();
-safeCreateFolder(datedTestDirectory);					
-shell.CurrentDirectory = datedTestDirectory;
-
-WScript.echo("Test results folder is " + datedTestDirectory);
-
-WScript.echo('');
-WScript.echo("======================");
-WScript.echo('Running tests...');
-WScript.echo('');
-WScript.echo("======================");
-WScript.echo('Now really running tests...');
-WScript.echo('');
-
-var batchCommand = '"' + baseDirectory + '/../packages/NUnit.2.5.10.11092/tools/nunit-console-x86.exe"'
-                    + ' "' + baseDirectory + '/../bin/debug/ExampleApp.Spec.dll"'
-					+ ' /labels /out=TestResult.txt /xml=TestResult.xml';
-WScript.echo(batchCommand);
-shell.run(batchCommand, 1 /* show normal */, true /* wait for this to finish*/);
-
-WScript.echo('Test complete.');
-WScript.echo('');
-WScript.echo("======================");
-WScript.echo(' Generating report...');
-WScript.echo('');
-var reportCommand = '"' + baseDirectory + '/../packages/SpecFlow.1.8.1/tools/specflow.exe"'
-                    + ' nunitexecutionreport' 
-					+ ' "' + baseDirectory + "/ExampleApp/ExampleApp.csproj";
-shell.run(reportCommand, 1 /* show normal */, true /* wait for this to finish*/);
-
-WScript.echo('Report complete.');
-WScript.echo('');
-WScript.echo("======================");
-WScript.echo('Moving images...');
-WScript.echo('');
-
-var imageSpec = baseDirectory + "/../bin/debug/_EmuShot_*.png";
-fso.MoveFile(imageSpec, datedTestDirectory);
-
-WScript.echo('Images moved.');
-WScript.echo('');
-WScript.echo("======================");
-WScript.echo('Manipulating image references in report...');
-WScript.echo('');
-
-var reportPath = datedTestDirectory + '/TestResult.html';
-var reportFile = fso.GetFile(reportPath);
-var reportText = readFileIntoString(reportFile);
-reportText = patchUpReport(reportText);
-writeStringIntoFile(reportFile, reportText);
-
-WScript.echo('Report image references corrected.');
-WScript.echo('');
-WScript.echo("======================");
-WScript.echo('Complete!');
-WScript.echo('');
-
-
-if (WScript.Arguments.Count() == 0 || WScript.Arguments[0] != '/s') {
-    WScript.echo('Opening test result file...');
-	shell.run(reportPath);
-}
-
-/*
-This code is NBG!
-
-var imageDirectory = baseDirectory + "/ExampleApp.Spec/bin/debug/";
-var folder = fso.GetFolder(imageDirectory);
-var countMoved = 0;
-for (var item in folder.Files){
-	if (item.Name.endsWith('.png') && item.Name.startsWith('_EmuShot_')) {
-		item.Move(datedTestDirectory + '/' + item.Name);
-		countMoved++;
-	}
-}
-WScript.echo(countMoved + ' image(s) moved.');
-*/
+// run
+executeTests(settings);
